5f802c4a3a6f8b256561ec1a8304e977c658adfc
Dmitry <root@uralbash.ru>
2015-07-16 17:16:36 +0500

    fix init test

--- elfinder/tests/__init__.py
+++ elfinder/tests/__init__.py
@@ -14,7 +14,6 @@ import os
 import json
 import unittest
 from pprint import pprint
-from datetime import datetime
 
 from elfinder import Connector, ElfinderException
 
@@ -59,36 +58,16 @@ class TestCommand(BaseTests):
         request = json.loads("""{"cmd": "open", "init": 1, "tree": 1}""")
         connector = Connector(self.options)
         response = connector.run(request)
-        date = datetime.fromtimestamp(
-            os.stat(ROOT).st_mtime
-        ).strftime("%d %b %Y %H:%M")
-        PP(response)
-        self.assertEqual(
-            response,
-            {'api': '2.0',
-             'cdc': [{'date': 'Today 19:10',
-                      'dim': '420x420',
-                      'hash': 'edefc1e0456cc87c4278bfa52c01851f',
-                      'mime': 'image/jpeg',
-                      'name': 'bear.jpeg',
-                      'read': True,
-                      'resize': True,
-                      'rm': True,
-                      'size': 37675,
-                      'url': '/bear.jpeg',
-                      'write': True}],
-             'cwd': {'date': date,
-                     'hash': '8320a3d391b236e4cbde269a665c3045',
-                     'mime': 'directory',
-                     'name': 'Home',
-                     'read': True,
-                     'rel': 'Home',
-                     'rm': False,
-                     'size': 0,
-                     'write': True},
-             'tree': {'dirs': [],
-                      'hash': '8320a3d391b236e4cbde269a665c3045',
-                      'name': 'Home',
-                      'read': True,
-                      'write': True}}
-        )
+        self.assertEqual(response['api'], '2.0')
+        self.assertEqual(len(response['cdc']), 1)
+        self.assertEqual(response['cwd']['mime'], 'directory')
+        self.assertEqual(response['cwd']['name'], 'Home')
+        self.assertEqual(response['cwd']['read'], True)
+        self.assertEqual(response['cwd']['write'], True)
+        self.assertEqual(response['cwd']['size'], 0)
+        self.assertEqual(response['cwd']['rel'], 'Home')
+        self.assertEqual(response['cwd']['rm'], False)
+        self.assertEqual(response['tree']['dirs'], [])
+        self.assertEqual(response['tree']['name'], 'Home')
+        self.assertEqual(response['tree']['read'], True)
+        self.assertEqual(response['tree']['write'], True)
